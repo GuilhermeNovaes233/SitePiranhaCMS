474290f823b4e6aa20ea20d3263483fae1f796a2
Guilherme Silva <guilherme.silva@codeblox.com.br>
2020-10-21 23:19:47 -0300

    Criando projeto e adicionando template

---
+++ Models/StandardPage.cs
@@ -0,0 +1,10 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace SitePiranhaCMS.Models
+{
+    [PageType(Title = "Standard page")]
+    public class StandardPage  : Page<StandardPage>
+    {
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Piranha;
+using Piranha.AttributeBuilder;
+using Piranha.AspNetCore.Identity.SQLite;
+using Piranha.Data.EF.SQLite;
+using Piranha.Manager.Editor;
+
+namespace SitePiranhaCMS
+{
+    public class Startup
+    {
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="configuration">The current configuration</param>
+        public Startup(IConfiguration configuration)
+        {
+            _config = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Service setup
+            services.AddPiranha(options =>
+            {
+                options.AddRazorRuntimeCompilation = true;
+
+                options.UseFileStorage(naming: Piranha.Local.FileStorageNaming.UniqueFolderNames);
+                options.UseImageSharp();
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseMemoryCache();
+                options.UseEF<SQLiteDb>(db =>
+                    db.UseSqlite(_config.GetConnectionString("piranha")));
+                options.UseIdentityWithSeed<IdentitySQLiteDb>(db =>
+                    db.UseSqlite(_config.GetConnectionString("piranha")));
+
+                /***
+                 * Here you can configure the different permissions
+                 * that you want to use for securing content in the
+                 * application.
+                options.UseSecurity(o =>
+                {
+                    o.UsePermission("WebUser", "Web User");
+                });
+                 */
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApi api)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Initialize Piranha
+            App.Init(api);
+
+            // Build content types
+            new ContentTypeBuilder(api)
+                .AddAssembly(typeof(Startup).Assembly)
+                .Build()
+                .DeleteOrphans();
+
+            // Configure Tiny MCE
+            EditorConfig.FromFile("editorconfig.json");
+
+            // Adicionando css 
+            //App.Modules.Manager()
+            //    .Styles.Add("~/assets/css/linearicons.css");
+            //App.Modules.Manager()
+            //    .Styles.Add("~/assets/css/bootstrap.cs");
+            //App.Modules.Manager()
+            //    .Styles.Add("~/assets/css/owl.carousel.css");
+            //App.Modules.Manager()
+            //   .Styles.Add("~/assets/css/font-awesome.min.cs");
+            //App.Modules.Manager()
+            //    .Styles.Add("~/assets/css/magnific-popup.css");
+            //App.Modules.Manager()
+            //    .Styles.Add("~/assets/css/nice-select.css");
+            //App.Modules.Manager()
+            //    .Styles.Add("~/assets/css/main.css");
+
+            // Middleware setup
+            app.UsePiranha(options => {
+                options.UseManager();
+                options.UseTinyMCE();
+                options.UseIdentity();
+            });
+        }
+    }
+}
