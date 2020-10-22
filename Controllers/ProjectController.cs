693a457c169fb1041b678a57299b97f3f41a577c
Guilherme Silva <guilherme.silva@codeblox.com.br>
2020-10-21 23:44:04 -0300

    Adicionando Controllers e views

---
+++ Controllers/ProjectController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SitePiranhaCMS.Web.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
